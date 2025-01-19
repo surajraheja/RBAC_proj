e7c96eabad4a0f501a600792bb3646509528c5cc
Suraj Raheja <surajraheja20@gmail.com>
2025-01-20 05:29:35 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,20 @@
-"""
-URL configuration for rbac_system project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+from .accessValidationAPIs import *
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'roles', RoleViewSet)
+router.register(r'permissions', PermissionViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),  # Include user management URLs
+    path('', include(router.urls)),
+    path('users/<int:pk>/', UserDeleteView.as_view(), name='delete-user'),
+    path('roles/<int:pk>/', RoleDeleteView.as_view(), name='delete-role'),
+    path('permissions/<int:pk>/', PermissionDeleteView.as_view(), name='delete-permission'),
+    path('api_one/', APIOneView.as_view(), name='api_one'),
+    path('api_two/', APITwoView.as_view(), name='api_two'),
+    path('api_three/', APIThreeView.as_view(), name='api_three'),
+    path("validate_access/", AccessValidationView.as_view(), name="validate_access"),
 ]
